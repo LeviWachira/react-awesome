765b4e212cff5080edca03e6f157320b86cef56f
LeviWachira <trojan.wachira@gmail.com>
2019-11-15 04:47:49 +0700

    edit index

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// git update
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import GetItem from './components/GetItem';
-
 
 AppRegistry.registerComponent(appName, () => App);
