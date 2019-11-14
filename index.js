28cd5b944dc4c2515496b26ebe9d28a68064695a
Wachira Winitchai <57758398+LeviWachira@users.noreply.github.com>
2019-11-15 04:29:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // aria gor yom
-<<<<<<< HEAD
 //pim thai mai dai
-=======
 // mai bok kern
->>>>>>> 4d558736605320f5203e5f235cb9f83f0b40d2d3
+
 
 import {AppRegistry} from 'react-native';
 import App from './App';
