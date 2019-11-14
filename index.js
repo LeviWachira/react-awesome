fca271c7535fba912cedb6306b26a0a2d3685f6a
LeviWachira <trojan.wachira@gmail.com>
2019-11-15 04:46:44 +0700

    .

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
+// git update
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import GetItem from './components/GetItem';
+
 
 AppRegistry.registerComponent(appName, () => App);
