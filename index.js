4c39bf103b4f6ea482ebe2556d4c53826f8fab60
LeviWachira <trojan.wachira@gmail.com>
2019-11-15 04:38:41 +0700

    last update conflict

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// aria gor yom
-//pim thai mai dai
-// mai bok kern
-
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import GetItem from './components/GetItem';
 
 AppRegistry.registerComponent(appName, () => App);
